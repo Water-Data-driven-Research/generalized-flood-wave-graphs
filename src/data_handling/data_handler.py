b3d243c6d311e63f48fbb365c3bd54e9e6fadfc7
Viktor Marsi <marsi0viktor@gmail.com>
2025-01-29 12:25:06 +0100

    station_river_mapping added

--- src/data_handling/data_handler.py
+++ src/data_handling/data_handler.py
@@ -22,6 +22,9 @@ class DataHandler:
         - rivers_unsorted: Dictionary: keys are river names and values are lists of station names
         lying along the river
 
+        - station_river_mapping: Dictionary: keys are station names and values are the corresponding
+        river names
+
         - river_connections: Dictionary: keys are river names, values are dictionaries
         like {"close_beginning": 0, "close_ending": 2275} (if the river has to be closed
         with station 2275)
@@ -34,6 +37,7 @@ class DataHandler:
         self.station_names = dict(self.dl.meta_data['station_name'])
         self.station_coordinates = self.get_station_coordinates()
         self.rivers_unsorted = self.get_rivers_unsorted()
+        self.station_river_mapping = self.get_station_river_mapping()
         self.river_connections = self.dl.river_connections
 
     def get_station_coordinates(self) -> dict:
@@ -68,3 +72,17 @@ class DataHandler:
             rivers_unsorted[river_name] = station_names_along_river
 
         return rivers_unsorted
+
+    def get_station_river_mapping(self) -> dict:
+        """
+        Creates the station-river mapping described in the constructor.
+        :return dict: station_river_mapping
+        """
+        station_river_mapping = {}
+        for station_name in self.station_names:
+            for river_name in list(self.rivers_unsorted.keys()):
+                if station_name in self.rivers_unsorted[river_name]:
+                    station_river_mapping[station_name] = river_name
+                    break
+
+        return station_river_mapping
