e0a338888000c5fee9a5bb9cfcc5ddc8721fec3d
Viktor Marsi <marsi0viktor@gmail.com>
2025-01-29 12:41:22 +0100

    stations

--- src/wng_building/station_river_creator.py
+++ src/wng_building/station_river_creator.py
@@ -1,3 +1,25 @@
+from src.data_handling.data_handler import DataHandler
+
+
 class StationRiverCreator:
-    def __init__(self):
-        pass
+    def __init__(self, data_handler: DataHandler):
+        self.data_handler = data_handler
+
+    def run(self):
+        stations = self.create_stations()
+
+    def create_stations(self):
+        stations = {}
+        for reg_num in list(self.data_handler.station_names.keys()):
+            station_name = self.data_handler.station_names[reg_num]
+            river_name = self.data_handler.station_river_mapping[station_name]
+
+            stations[reg_num] = {
+                'river_name': river_name,
+                'station_name': station_name,
+                'EOVy': self.data_handler.station_coordinates['EOVy'],
+                'EOVx': self.data_handler.station_coordinates['EOVx'],
+                'null_point': self.data_handler.station_coordinates['null_point']
+            }
+
+        return stations
