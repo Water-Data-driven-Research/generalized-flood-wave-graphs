886a71f9f63dae01ee049628d6efe0da8b8e4359
Viktor Marsi <marsi0viktor@gmail.com>
2025-01-30 09:05:09 +0100

    rivers and completed rivers are saved into different files

--- src/data_handling/generated_data_handler.py
+++ src/data_handling/generated_data_handler.py
@@ -7,21 +7,24 @@ import networkx as nx
 class GeneratedDataHandler:
 
     @staticmethod
-    def save_pickle(vertices: list, edges: list,
-                    data_folder_path: str, file_name: str):
-        os.makedirs(os.path.join(data_folder_path, 'generated'), exist_ok=True)
+    def save_pickle(generated_path: str, folder_name: str,
+                    file_name: str,
+                    vertices: list = None, edges: list = None,
+                    graph: nx.DiGraph = None) -> None:
+        os.makedirs(os.path.join(generated_path, folder_name), exist_ok=True)
 
-        graph = nx.DiGraph()
+        if graph is None:
+            graph = nx.DiGraph()
+            graph.add_nodes_from(vertices)
+            graph.add_edges_from(edges)
 
-        graph.add_nodes_from(vertices)
-        graph.add_edges_from(edges)
-
-        with open(os.path.join(data_folder_path, 'generated', f'{file_name}.pkl'), 'wb') as f:
+        with open(os.path.join(generated_path, folder_name, f'{file_name}.pkl'), 'wb') as f:
             pickle.dump(graph, f)
 
     @staticmethod
-    def read_pickle(data_folder_path: str, file_name: str) -> nx.DiGraph:
-        with open(os.path.join(data_folder_path, 'generated', f'{file_name}.pkl'), 'rb') as f:
+    def read_pickle(generated_path: str, folder_name: str,
+                    file_name: str) -> nx.DiGraph:
+        with open(os.path.join(generated_path, folder_name, f'{file_name}.pkl'), 'rb') as f:
             graph = pickle.load(f)
 
         return graph
--- src/wng_building/water_network_graph_builder.py
+++ src/wng_building/water_network_graph_builder.py
@@ -1,3 +1,7 @@
+import os
+
+import networkx as nx
+
 from src.data_handling.data_handler import DataHandler
 from src.data_handling.dataloader import DataLoader
 from src.data_handling.generated_data_handler import GeneratedDataHandler
@@ -11,42 +15,66 @@ class WaterNetworkGraphBuilder:
         self.data_handler = DataHandler(dl=self.dl)
         self.station_river_creator = StationRiverCreator(data_handler=self.data_handler)
 
+        self.generated_path = os.path.join(self.dl.data_folder_path, 'generated')
+
     def run(self):
         self.station_river_creator.run()
 
         vertices = self.get_vertices()
         self.get_rivers_graph(vertices=vertices)
-        self.get_water_network_graph(vertices=vertices)
+        self.get_completed_rivers(vertices=vertices)
+        self.get_water_network_graph()
 
     def get_vertices(self) -> list:
         vertices = list(self.station_river_creator.stations.keys())
 
         GeneratedDataHandler.save_pickle(
             vertices=vertices, edges=[],
-            data_folder_path=self.dl.data_folder_path,
+            generated_path=self.generated_path,
+            folder_name='vertices',
             file_name='vertices'
         )
 
         return vertices
 
     def get_rivers_graph(self, vertices: list) -> None:
-        edges = []
-        for river in list(self.station_river_creator.rivers.values()):
-            edges = edges + list(zip(river, river[1:]))
+        for river_name in list(self.station_river_creator.rivers.keys()):
+            river = self.station_river_creator.rivers[river_name]
+            edges = list(zip(river, river[1:]))
+
+            GeneratedDataHandler.save_pickle(
+                vertices=vertices, edges=edges,
+                generated_path=self.generated_path,
+                folder_name='rivers',
+                file_name=river_name
+            )
+
+    def get_completed_rivers(self, vertices: list) -> None:
+        for river_name in list(self.station_river_creator.completed_rivers.keys()):
+            river = self.station_river_creator.completed_rivers[river_name]
+            edges = list(zip(river, river[1:]))
+
+            GeneratedDataHandler.save_pickle(
+                vertices=vertices, edges=edges,
+                generated_path=self.generated_path,
+                folder_name='completed_rivers',
+                file_name=f'cl_{river_name}'
+            )
+
+    def get_water_network_graph(self):
+        water_network_graph = nx.DiGraph()
+        for river_name in list(self.station_river_creator.completed_rivers.keys()):
+            completed_river = GeneratedDataHandler.read_pickle(
+                generated_path=self.generated_path,
+                folder_name='completed_rivers',
+                file_name=f'cl_{river_name}'
+            )
+
+            water_network_graph = nx.compose(water_network_graph, completed_river)
 
         GeneratedDataHandler.save_pickle(
-            vertices=vertices, edges=edges,
-            data_folder_path=self.dl.data_folder_path,
-            file_name='rivers'
-        )
-
-    def get_water_network_graph(self, vertices: list) -> None:
-        edges = []
-        for river in list(self.station_river_creator.completed_rivers.values()):
-            edges = edges + list(zip(river, river[1:]))
-
-        GeneratedDataHandler.save_pickle(
-            vertices=vertices, edges=edges,
-            data_folder_path=self.dl.data_folder_path,
-            file_name='water_network_graph'
+            graph=water_network_graph,
+            generated_path=self.generated_path,
+            folder_name='water_network_graph',
+            file_name=f'wng'
         )
