c5f52a5f6b15c6ca639dbfc2769fadabc9b3f692
Viktor Marsi <marsi0viktor@gmail.com>
2025-01-31 13:59:30 +0100

    docstrings

--- src/data_handling/data_interface.py
+++ src/data_handling/data_interface.py
@@ -1,5 +1,11 @@
 class DataInterface:
+    """
+    Class for storing data created in DataHandler.
+    """
     def __init__(self):
+        """
+        Constructor.
+        """
         self.time_series_data = None
         self.reg_station_mapping = None
         self.station_reg_mapping = None
