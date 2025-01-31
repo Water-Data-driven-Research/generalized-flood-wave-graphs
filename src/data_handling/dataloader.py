e0ed40f7f83afdc4c6027670d38e5ead804e4c68
Viktor Marsi <marsi0viktor@gmail.com>
2025-01-31 13:36:31 +0100

    DataDownloader

--- src/data_handling/dataloader.py
+++ src/data_handling/dataloader.py
@@ -1,13 +1,15 @@
 import json
 import os
 
-import gdown
 import pandas as pd
 
-from src import PROJECT_PATH
+from src.data_handling.data_downloader import DataDownloader
 
 
 class DataLoader:
+    """
+    Class for loading all necessary data.
+    """
     def __init__(self,
                  do_download: bool = False,
                  folder_link: str = None,
@@ -16,32 +18,23 @@ class DataLoader:
         Constructor.
         :param bool do_download: True if we wish to download data, False if we don't
         :param str folder_link: The link of the folder containing all necessary data
-        :param str data_folder_path:
+        :param str data_folder_path: the location where we wish to place the data folder
         """
         self.do_download = do_download
-        self.folder_link = folder_link
-        self.data_folder_path = data_folder_path
 
         self.dataset_name = 'time_series_data'
 
-        if self.data_folder_path is None:
-            self.data_folder_path = os.path.join(PROJECT_PATH, 'data')
+        self.ddl = DataDownloader(folder_link=folder_link, data_folder_path=data_folder_path)
 
         if self.do_download:
-            self.download_data()
+            self.ddl.download_data()
 
         self.time_series_data = None
         self.meta_data = None
         self.river_connections = None
-        self.read_data()
+        self.load_data()
 
-    def download_data(self) -> None:
-        """
-        Downloads all data from Google Drive.
-        """
-        gdown.download_folder(url=self.folder_link, output=self.data_folder_path)
-
-    def read_data(self) -> None:
+    def load_data(self) -> None:
         """
         Reads downloaded data from the data folder and saves them in member variables.
         """
@@ -50,16 +43,16 @@ class DataLoader:
         river_connections_file_name = 'river_connections.json'
 
         self.time_series_data = pd.read_csv(
-            os.path.join(self.data_folder_path, time_series_file_name),
+            os.path.join(self.ddl.data_folder_path, time_series_file_name),
             index_col=[0]
         )
 
         self.meta_data = pd.read_csv(
-            os.path.join(self.data_folder_path, meta_file_name),
+            os.path.join(self.ddl.data_folder_path, meta_file_name),
             index_col=[5]
         )
 
         with open(
-                os.path.join(self.data_folder_path, river_connections_file_name)
+                os.path.join(self.ddl.data_folder_path, river_connections_file_name)
         ) as f:
             self.river_connections = json.load(f)
