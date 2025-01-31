e08e1dde0aed852b0ca46cb4740ca56c76fd51a5
Viktor Marsi <marsi0viktor@gmail.com>
2025-01-31 13:58:16 +0100

    small changes

--- src/wng_building/station_river_creator.py
+++ src/wng_building/station_river_creator.py
@@ -1,36 +1,34 @@
 import copy
 
-from src.data_handling.data_handler import DataHandler
+from src.data_handling.data_interface import DataInterface
+from src.wng_building.station_river_data_interface import StationRiverDataInterface
 
 
 class StationRiverCreator:
     """
     Class for creating the following data structures: stations, rivers, completed rivers
     """
-    def __init__(self, data_handler: DataHandler):
+    def __init__(self, data_interface: DataInterface):
         """
         Constructor.
-        :param DataHandler data_handler: a DataHandler instance
+        :param DataInterface data_interface: a DataInterface instance
         """
-        self.data_handler = data_handler
+        self.data = data_interface
 
-        self.stations = None
-        self.rivers = None
-        self.completed_rivers = None
+        self.interface = StationRiverDataInterface()
 
     def run(self) -> None:
         """
         Run function. Gets stations, rivers and completed rivers.
         """
-        self.data_handler.run()
 
-        self.stations = self.create_stations()
+        self.create_stations()
 
-        self.rivers = self.create_rivers()
+        self.create_rivers()
 
-        self.completed_rivers = self.create_completed_rivers()
+        self.create_completed_rivers()
 
-    def create_stations(self) -> dict:
+    def create_stations(self) -> None:
         """
         Function for creating the stations data structure.
         stations is a dictionary with reg-number keys and dictionary values like
@@ -41,33 +39,49 @@ class StationRiverCreator:
             'EOVx': 101317.2,
             'null_point': 73.70
         }
-        :return dict: dictionary of all stations
         """
         stations = {}
-        for reg_num in list(self.data_handler.reg_station_mapping.keys()):
-            station_name = self.data_handler.reg_station_mapping[reg_num]
-            river_name = self.data_handler.station_river_mapping[station_name]
+        for reg_num in list(self.data.reg_station_mapping.keys()):
+            station_name = self.data.reg_station_mapping[reg_num]
+            river_name = self.data.station_river_mapping[station_name]
 
             stations[reg_num] = {
                 'river_name': river_name,
                 'station_name': station_name,
-                'EOVy': self.data_handler.station_coordinates[reg_num]['EOVy'],
-                'EOVx': self.data_handler.station_coordinates[reg_num]['EOVx'],
-                'null_point': self.data_handler.station_coordinates[reg_num]['null_point']
+                'EOVy': self.data.station_coordinates[reg_num]['EOVy'],
+                'EOVx': self.data.station_coordinates[reg_num]['EOVx'],
+                'null_point': self.data.station_coordinates[reg_num]['null_point']
             }
 
-        return stations
+        self.interface.stations = stations
 
-    def create_rivers(self) -> dict:
+    def create_rivers(self) -> None:
         """
         Function for creating the rivers data structure. A river looks like
         {'river_name': [station1, station2, ..., stationN]}, where the stations
         inside the list are sorted in descending order by their null points.
-        :return dict: dictionary of all rivers
         """
         rivers_unsorted = self.get_rivers_unsorted()
+        rivers_sorted = self.sort_rivers(rivers_unsorted=rivers_unsorted)
 
-        return self.sort_rivers(rivers_unsorted=rivers_unsorted)
+        self.interface.rivers = rivers_sorted
+
+    def create_completed_rivers(self) -> None:
+        """
+        Creates completed rivers. Uses river_connections to append the rivers with some
+        "completing" stations.
+        """
+        completed_rivers = copy.deepcopy(self.interface.rivers)
+        for river_name in list(self.data.river_connections.keys()):
+            close_beginning = self.data.river_connections[river_name]['close_beginning']
+            close_ending = self.data.river_connections[river_name]['close_ending']
+
+            if close_beginning is not None:
+                completed_rivers[river_name] = [close_beginning] + completed_rivers[river_name]
+            if close_ending is not None:
+                completed_rivers[river_name] = completed_rivers[river_name] + [close_ending]
+
+        self.interface.completed_rivers = completed_rivers
 
     def get_rivers_unsorted(self) -> dict:
         """
@@ -77,10 +91,10 @@ class StationRiverCreator:
         :return dict: dictionary of unsorted rivers
         """
         rivers_unsorted = {}
-        for river_name in list(self.data_handler.river_station_mapping.keys()):
-            station_name_list = self.data_handler.river_station_mapping[river_name]
+        for river_name in list(self.data.river_station_mapping.keys()):
+            station_name_list = self.data.river_station_mapping[river_name]
             reg_number_list = [
-                self.data_handler.station_reg_mapping[station_name] for station_name in station_name_list
+                self.data.station_reg_mapping[station_name] for station_name in station_name_list
             ]
 
             rivers_unsorted[river_name] = reg_number_list
@@ -98,27 +112,9 @@ class StationRiverCreator:
             river_unsorted = rivers_unsorted[river_name]
             river_sorted = sorted(
                 river_unsorted,
-                key=lambda x: -self.data_handler.station_coordinates[x]['null_point']
+                key=lambda x: -self.data.station_coordinates[x]['null_point']
             )
 
             rivers_sorted[river_name] = river_sorted
 
         return rivers_sorted
-
-    def create_completed_rivers(self) -> dict:
-        """
-        Creates completed rivers. Uses river_connections to append the rivers with some
-        "completing" stations.
-        :return dict: dictionary of completed rivers
-        """
-        completed_rivers = copy.deepcopy(self.rivers)
-        for river_name in list(self.data_handler.river_connections.keys()):
-            close_beginning = self.data_handler.river_connections[river_name]['close_beginning']
-            close_ending = self.data_handler.river_connections[river_name]['close_ending']
-
-            if close_beginning != 0:
-                completed_rivers[river_name] = [close_beginning] + completed_rivers[river_name]
-            if close_ending != 0:
-                completed_rivers[river_name] = completed_rivers[river_name] + [close_ending]
-
-        return completed_rivers
--- src/wng_building/station_river_data_interface.py
+++ src/wng_building/station_river_data_interface.py
@@ -1,5 +1,11 @@
 class StationRiverDataInterface:
+    """
+    Class for storing data created in StationRiverCreator.
+    """
     def __init__(self):
+        """
+        Constructor.
+        """
         self.stations = None
         self.rivers = None
         self.completed_rivers = None
