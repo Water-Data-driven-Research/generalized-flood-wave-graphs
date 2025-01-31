b037a47789b0befe1de1589905bf1f35412fbad6
Viktor Marsi <marsi0viktor@gmail.com>
2025-01-31 13:12:05 +0100

    run function runs in the constructor and DataInterface was created

---
+++ src/data_handling/data_interface.py
@@ -0,0 +1,9 @@
+class DataInterface:
+    def __init__(self):
+        self.time_series_data = None
+        self.reg_station_mapping = None
+        self.station_reg_mapping = None
+        self.station_coordinates = None
+        self.river_station_mapping = None
+        self.station_river_mapping = None
+        self.river_connections = None
