3377be5ce377e852a8f250194c57049257e95558
Viktor Marsi <marsi0viktor@gmail.com>
2025-02-03 14:41:52 +0100

    filling interface nicely

--- src/wng_building/station_river_creator.py
+++ src/wng_building/station_river_creator.py
@@ -8,27 +8,29 @@ class StationRiverCreator:
     """
     Class for creating the following data structures: stations, rivers, completed rivers
     """
-    def __init__(self, data_interface: DataInterface):
+    def __init__(self, data_if: DataInterface):
         """
         Constructor.
-        :param DataInterface data_interface: a DataInterface instance
+        :param DataInterface data_if: a DataInterface instance
         """
-        self.data_if = data_interface
+        self.data_if = data_if
 
-        self.interface = StationRiverDataInterface()
+        self.station_river_if = StationRiverDataInterface()
 
     def run(self) -> None:
         """
         Run function. Gets stations, rivers and completed rivers.
         """
+        rivers = self.create_rivers()
+        data = {
+            'stations': self.create_stations(),
+            'rivers': rivers,
+            'completed_rivers': self.create_completed_rivers(rivers=rivers)
+        }
 
-        self.create_stations()
+        self.station_river_if = StationRiverDataInterface(data=data)
 
-        self.create_rivers()
-
-        self.create_completed_rivers()
-
-    def create_stations(self) -> None:
+    def create_stations(self) -> dict:
         """
         Function for creating the stations data structure.
         stations is a dictionary with reg-number keys and dictionary values like
@@ -39,6 +41,7 @@ class StationRiverCreator:
             'EOVx': 101317.2,
             'null_point': 73.70
         }
+        :return dict: dictionary containing the stations
         """
         stations = {}
         for reg_num in list(self.data_if.reg_station_mapping.keys()):
@@ -53,25 +56,28 @@ class StationRiverCreator:
                 'null_point': self.data_if.station_coordinates[reg_num]['null_point']
             }
 
-        self.interface.stations = stations
+        return stations
 
-    def create_rivers(self) -> None:
+    def create_rivers(self) -> dict:
         """
         Function for creating the rivers data structure. A river looks like
         {'river_name': [station1, station2, ..., stationN]}, where the stations
         inside the list are sorted in descending order by their null points.
+        :return dict: dictionary containing the rivers
         """
         rivers_unsorted = self.get_rivers_unsorted()
         rivers_sorted = self.sort_rivers(rivers_unsorted=rivers_unsorted)
 
-        self.interface.rivers = rivers_sorted
+        return rivers_sorted
 
-    def create_completed_rivers(self) -> None:
+    def create_completed_rivers(self, rivers: dict) -> dict:
         """
         Creates completed rivers. Uses river_connections to append the rivers with some
         "completing" stations.
+        :return dict rivers: dictionary containing the rivers
+        :return dict: dictionary containing the completed rivers
         """
-        completed_rivers = copy.deepcopy(self.interface.rivers)
+        completed_rivers = copy.deepcopy(rivers)
         for river_name in list(self.data_if.river_connections.keys()):
             close_beginning = self.data_if.river_connections[river_name]['close_beginning']
             close_ending = self.data_if.river_connections[river_name]['close_ending']
@@ -81,7 +87,7 @@ class StationRiverCreator:
             if close_ending is not None:
                 completed_rivers[river_name] = completed_rivers[river_name] + [close_ending]
 
-        self.interface.completed_rivers = completed_rivers
+        return completed_rivers
 
     def get_rivers_unsorted(self) -> dict:
         """
