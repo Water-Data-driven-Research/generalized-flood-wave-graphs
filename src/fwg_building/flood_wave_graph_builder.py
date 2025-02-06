fbe25a3842b24a2809accf13145dfdb0ea9ad55e
Viktor Marsi <marsi0viktor@gmail.com>
2025-02-06 12:23:51 +0100

    docstrings

--- src/fwg_building/flood_wave_graph_builder.py
+++ src/fwg_building/flood_wave_graph_builder.py
@@ -6,8 +6,17 @@ from src.fwg_building.fwg_preparer_data_interface import FWGPreparerDataInterfac
 
 
 class FloodWaveGraphBuilder:
+    """
+    Class for building the Flood Wave Graph.
+    """
     def __init__(self, preparer_interface: FWGPreparerDataInterface,
-                 do_save_fwg: bool, data_folder_path: str):
+                 do_save_fwg: bool = False, data_folder_path: str = None):
+        """
+        Constructor.
+        :param FWGPreparerDataInterface preparer_interface: a FWGPreparerDataInterface instance
+        :param bool do_save_fwg: whether to save the Flood Wave Graph or not
+        :param str data_folder_path: path of the data folder
+        """
         self.preparer_if = preparer_interface
         self.do_save_fwg = do_save_fwg
         self.data_folder_path = data_folder_path
@@ -15,18 +24,28 @@ class FloodWaveGraphBuilder:
         self.fwg_if = FWGDataInterface()
 
     def run(self) -> None:
+        """
+        Run function. Builds the Flood Wave Graph and saves it if needed.
+        """
         self.fwg_if.fwg = self.build_flood_wave_graph()
 
         if self.do_save_fwg:
             self.save_fwg()
 
     def build_flood_wave_graph(self) -> nx.DiGraph:
+        """
+        Builds the Flood Wave Graph using networkx.
+        :return nx.DiGraph: the Flood Wave Graph
+        """
         fwg = nx.DiGraph()
         fwg.add_edges_from(self.preparer_if.edges)
 
         return fwg
 
     def save_fwg(self) -> None:
+        """
+        Saves the Flood Wave Graph.
+        """
         GeneratedDataLoader.save_pickle(
             graph=self.fwg_if.fwg,
             data_folder_path=self.data_folder_path,
--- src/fwg_building/flood_wave_graph_preparer.py
+++ src/fwg_building/flood_wave_graph_preparer.py
@@ -7,8 +7,20 @@ from src.fwg_building.fwg_preparer_data_interface import FWGPreparerDataInterfac
 
 
 class FloodWaveGraphPreparer:
+    """
+    Class for finding the nodes and edges of the Flood Wave Graph.
+    """
     def __init__(self, time_series_data: pd.DataFrame, completed_rivers: dict,
                  beta: int, delta: int):
+        """
+        Constructor.
+        :param pd.DataFrame time_series_data: dataframe containing all water level time series
+        :param dict completed_rivers: dictionary containing all completed rivers
+        :param int beta: hyperparameter for setting the maximal allowed time difference (in days)
+        between two connected nodes
+        :param int delta: hyperparameter for setting the lengths of the time intervals in which
+        we are looking for a peak value
+        """
         self.time_series_data = time_series_data
         self.completed_rivers = completed_rivers
         self.beta = beta
@@ -16,7 +28,11 @@ class FloodWaveGraphPreparer:
 
         self.preparer_if = FWGPreparerDataInterface()
 
-    def run(self):
+    def run(self) -> None:
+        """
+        Run function. Finds delta peaks and edges and saves them into the member variables
+        of the interface.
+        """
         delta_peak_bools = self.find_delta_peaks()
         data = {
             'delta_peaks': delta_peak_bools,
@@ -26,6 +42,11 @@ class FloodWaveGraphPreparer:
         self.preparer_if = FWGPreparerDataInterface(data=data)
 
     def find_delta_peaks(self) -> pd.DataFrame:
+        """
+        Finds delta-peaks using pandas.
+        :return pd.DataFrame: Data frame containing True and False values. True means delta-peak,
+        False means not delta-peak.
+        """
         peaks = pd.DataFrame(
             True,
             index=self.time_series_data.index,
@@ -41,7 +62,12 @@ class FloodWaveGraphPreparer:
 
         return peaks
 
-    def find_edges(self, delta_peak_bools: pd.DataFrame):
+    def find_edges(self, delta_peak_bools: pd.DataFrame) -> list:
+        """
+        Finds all edges of the FWG.
+        :param pd.DataFrame delta_peak_bools: delta-peaks data frame
+        :return list: all edges in a list
+        """
         all_edges = []
         for completed_river_name in self.completed_rivers:
             completed_river = self.completed_rivers[completed_river_name]
@@ -58,11 +84,18 @@ class FloodWaveGraphPreparer:
         return all_edges
 
     def find_edges_along_completed_river(self, completed_river: list, peaks: pd.DataFrame) -> list:
-        edges = []
+        """
+        Finds edges along a single completed river.
+        :param list completed_river: sorted list of stations in the completed river
+        :param pd.DataFrame peaks: delta-peak data frame
+        :return list: edges along the completed river
+        """
+        final_edges = []
         for start, end in zip(completed_river[:-1], completed_river[1:]):
             start_dates = peaks[start].loc[peaks[start]].index
             end_dates = peaks[end].loc[peaks[end]].index
             for date in start_dates:
+                # condition for being an edge
                 filtered_ends = end_dates[
                     (date <= end_dates) & \
                     (end_dates <= date + datetime.timedelta(days=self.beta))
@@ -72,15 +105,16 @@ class FloodWaveGraphPreparer:
                 )
                 date_datetime = datetime.datetime.strftime(date, "%Y-%m-%d")
 
+                # final structure of edges
                 start_node = [(start, date_datetime)]
                 end_nodes = list(product(
                     [end], list(filtered_ends_datetime)
                 ))
 
-                final_nodes = list(product(
+                edges = list(product(
                     start_node, end_nodes
                 ))
 
-                edges.extend(final_nodes)
+                final_edges.extend(edges)
 
-        return edges
+        return final_edges
