97f2d65c0c5736757862e3d08ded3121a3ca1600
Viktor Marsi <marsi0viktor@gmail.com>
2025-02-13 16:31:34 +0100

    FWGSelector constructor

--- src/analysis/fwg_selector.py
+++ src/analysis/fwg_selector.py
@@ -1,3 +1,16 @@
 class FWGSelector:
-    def __init__(self):
+    """
+    Class for spatial and temporal filtering of the Flood Wave Graph.
+    """
+    def __init__(self, graphs: dict, spatial_filtering: dict, temporal_filtering: dict):
+        """
+        Constructor.
+        :param dict graphs: {'fwg': flood_wave_graph, 'wng': water_network_graph}
+        :param dict temporal_filtering: {'start_date': start_date, 'end_date': end_date}
+        """
+        self.graphs = graphs
+        self.spatial_filtering = spatial_filtering
+        self.temporal_filtering = temporal_filtering
+
+    def run(self):
         pass
