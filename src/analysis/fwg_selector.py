f3f8a5ed865878d6a629aba2c07797a9eede1ffb
Viktor Marsi <marsi0viktor@gmail.com>
2025-02-13 18:08:14 +0100

    path selector done

--- src/analysis/fwg_selector.py
+++ src/analysis/fwg_selector.py
@@ -1,16 +1,52 @@
+import networkx as nx
+
+from src.data_handling.generated_dataloader import GeneratedDataLoader
+
+
 class FWGSelector:
     """
     Class for spatial and temporal filtering of the Flood Wave Graph.
     """
-    def __init__(self, graphs: dict, spatial_filtering: dict, temporal_filtering: dict):
+    def __init__(self, temporal_filtering: dict,
+                 data_folder_path: str,
+                 fwg: nx.DiGraph = None, wng: nx.DiGraph = None):
         """
         Constructor.
-        :param dict graphs: {'fwg': flood_wave_graph, 'wng': water_network_graph}
         :param dict temporal_filtering: {'start_date': start_date, 'end_date': end_date}
         """
-        self.graphs = graphs
-        self.spatial_filtering = spatial_filtering
+        self.fwg = fwg
+        self.wng = wng
+        self.data_folder_path = data_folder_path
         self.temporal_filtering = temporal_filtering
 
+        if self.fwg is None:
+            self.fwg = GeneratedDataLoader.read_pickle(
+                data_folder_path=self.data_folder_path,
+                folder_name='flood_wave_graph',
+                file_name='fwg'
+            )
+
+        if self.wng is None:
+            self.wng = GeneratedDataLoader.read_pickle(
+                data_folder_path=self.data_folder_path,
+                folder_name='water_network_graph',
+                file_name='wng'
+            )
+
+        self.wng_subgraph = nx.DiGraph()
+        self.fwg_subgraph = nx.DiGraph()
+
     def run(self):
-        pass
+        self.fwg_subgraph = self.get_fwg_subgraph()
+
+    def get_fwg_subgraph(self):
+        nodes_to_keep = []
+        for node in self.fwg.nodes:
+            condition_one = int(node[0]) in self.wng_subgraph.nodes
+            condition_two = (
+                    self.temporal_filtering['start_date'] <= node[1] <= self.temporal_filtering['end_date'])
+
+            if condition_one and condition_two:
+                nodes_to_keep.append(node)
+
+        return self.fwg.subgraph(nodes_to_keep).copy()
---
+++ src/analysis/wng_path_fwg_selector.py
@@ -0,0 +1,37 @@
+import networkx as nx
+
+from src.analysis.fwg_selector import FWGSelector
+
+
+class WNGPathFWGSelector(FWGSelector):
+    def __init__(self, data_folder_path: str,
+                 spatial_filtering: dict, temporal_filtering: dict,
+                 fwg: nx.DiGraph = None, wng: nx.DiGraph = None):
+        super().__init__(
+            temporal_filtering=temporal_filtering,
+            data_folder_path=data_folder_path,
+            fwg=fwg, wng=wng
+        )
+        self.spatial_filtering = spatial_filtering
+
+    def run(self):
+        self.wng_subgraph = self.get_wng_path()
+
+        super().run()
+
+    def get_wng_path(self) -> nx.DiGraph:
+        all_paths = list(nx.all_simple_paths(
+            self.wng,
+            source=int(self.spatial_filtering['source']),
+            target=int(self.spatial_filtering['target'])
+        ))
+
+        for path in all_paths:
+            if set(self.spatial_filtering['through']).issubset(path):
+                path_graph = nx.DiGraph()
+                path_graph.add_nodes_from(path)
+
+                return path_graph
+
+        raise Exception('There is no path from the source to the target that goes through' 
+                        'all given nodes.')
