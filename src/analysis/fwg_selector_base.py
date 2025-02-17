1c968eed41a32a17f1bc129e96e04fe3aa6aa419
Viktor Marsi <marsi0viktor@gmail.com>
2025-02-17 10:02:08 +0100

    docstrings

--- src/analysis/fwg_selector_base.py
+++ src/analysis/fwg_selector_base.py
@@ -1,4 +1,7 @@
+from typing import Any, Hashable
+
 import networkx as nx
+from networkx import Graph
 
 from src.data_handling.generated_dataloader import GeneratedDataLoader
 
@@ -13,6 +16,9 @@ class FWGSelectorBase:
         """
         Constructor.
         :param dict temporal_filtering: {'start_date': start_date, 'end_date': end_date}
+        :param str data_folder_path: path of the data folder
+        :param nx.DiGraph fwg: the Flood Wave Graph
+        :param nx.DiGraph wng: the Water Network Graph
         """
         self.fwg = fwg
         self.wng = wng
@@ -36,10 +42,18 @@ class FWGSelectorBase:
         self.wng_subgraph = nx.DiGraph()
         self.fwg_subgraph = nx.DiGraph()
 
-    def run(self):
+    def run(self) -> None:
+        """
+        Run function. Gets the desired subgraph of the FWG.
+        """
         self.fwg_subgraph = self.get_fwg_subgraph()
 
-    def get_fwg_subgraph(self):
+    def get_fwg_subgraph(self) -> Graph[Hashable | Any]:
+        """
+        Gets the subgraph by keeping only those (reg_num, date) nodes for which reg_num is
+        a node of the WNG and date is between start_date and end_date.
+        :return Graph[Hashable | Any]: the desired subgraph of the FWG
+        """
         nodes_to_keep = []
         for node in self.fwg.nodes:
             condition_one = node[0] in self.wng_subgraph.nodes
