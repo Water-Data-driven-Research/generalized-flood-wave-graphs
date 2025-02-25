559eea08be8d6055ef997ecaa1f1bccdd07e3219
Viktor Marsi <marsi0viktor@gmail.com>
2025-02-25 13:08:50 +0100

    selector

--- src/analysis/flood_wave_selector.py
+++ src/analysis/flood_wave_selector.py
@@ -1,3 +1,66 @@
+from datetime import datetime
+
+
 class FloodWaveSelector:
-    def __init__(self):
-        pass
+
+    @staticmethod
+    def get_flood_waves_from_start_to_end(waves: list, impacted_stations: list,
+                                          equivalence: bool) -> list:
+        """
+        Selects only those flood waves that impacted all stations in impacted_stations.
+        :param list waves: list of all the flood waves
+        :param list impacted_stations: stations the flood waves should go through
+        :param bool equivalence: True if we only consider one element of the equivalence
+        classes, False otherwise
+        :return list: full flood waves
+        """
+        if equivalence:
+            final_waves = []
+            for wave in waves:
+                stations_in_flood_wave = [wave[i][0] for i in range(len(wave))]
+                if set(impacted_stations).issubset(stations_in_flood_wave):
+                    final_waves.append(wave)
+
+        else:
+            final_waves = []
+            for paths in waves:
+                filtered_paths = []
+                for path in paths:
+                    stations_in_flood_wave = [path[i][0] for i in range(len(path))]
+                    if set(impacted_stations).issubset(stations_in_flood_wave):
+                        filtered_paths.append(path)
+                        final_waves.append(filtered_paths)
+
+        return final_waves
+
+    @staticmethod
+    def get_flood_waves_by_duration(waves: list, max_duration_days: int,
+                                    equivalence: bool) -> list:
+        """
+        Gets flood waves for which the time difference between the first and last nodes
+        are at most max_duration_days.
+        :param list waves: list of all the flood waves
+        :param int max_duration_days: maximal allowed time duration of a flood wave
+        :param bool equivalence: True if we only consider one element of the equivalence
+        classes, False otherwise
+        :return:
+        """
+        if equivalence:
+            final_waves = []
+            for wave in waves:
+                date1 = datetime.strptime(wave[0][1], "%Y-%m-%d")
+                date2 = datetime.strptime(wave[-1][1], "%Y-%m-%d")
+                days_diff = (date2 - date1).days
+                if days_diff <= max_duration_days:
+                    final_waves.append(wave)
+
+        else:
+            final_waves = []
+            for paths in waves:
+                date1 = datetime.strptime(paths[0][0][1], "%Y-%m-%d")
+                date2 = datetime.strptime(paths[0][-1][1], "%Y-%m-%d")
+                days_diff = (date2 - date1).days
+                if days_diff <= max_duration_days:
+                    final_waves.append(paths)
+
+        return final_waves
