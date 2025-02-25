23b006f8a4b6b2fad500a207a0d2df512e5bce7f
Viktor Marsi <marsi0viktor@gmail.com>
2025-02-25 14:36:19 +0100

    run

--- src/analysis/flood_wave_extractor.py
+++ src/analysis/flood_wave_extractor.py
@@ -8,18 +8,30 @@ class FloodWaveExtractor:
     This class is responsible for extracting the flood waves from a given FWG
     """
     def __init__(self, fwg: nx.DiGraph, wng: nx.DiGraph,
-                 station_coordinates: dict):
+                 station_coordinates: dict, equivalence: bool):
         """
         Constructor.
         :param nx.DiGraph fwg: the filtered Flood Wave Graph
         :param nx.DiGraph wng: the filtered Water Network Graph
         :param dict station_coordinates: coordinates of each station in a dictionary
+        :param bool equivalence: True if we only consider one element of the equivalence
+        classes, False otherwise
         """
         self.fwg = fwg
         self.wng = wng
         self.station_coordinates = station_coordinates
+        self.equivalence = equivalence
 
         self.flood_waves = None
+
+    def run(self) -> None:
+        """
+        Run function. Gets flood waves.
+        """
+        if self.equivalence:
+            self.get_flood_waves()
+        else:
+            self.get_flood_waves_without_equivalence()
 
     def get_flood_waves(self):
         """
