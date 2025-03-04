9c48a937f34efe81c74f08f6e870c06dab700b10
Viktor Marsi <marsi0viktor@gmail.com>
2025-03-04 17:36:48 +0100

    fwg subgraph instead of the whole fwg

--- src/analysis/wng_sink_fwg_plot_preparer.py
+++ src/analysis/wng_sink_fwg_plot_preparer.py
@@ -1,11 +1,12 @@
+import networkx as nx
+
 from src.data_handling.data_interface import DataInterface
-from src.fwg_building.fwg_data_interface import FWGDataInterface
 
 
 class WNGSinkFWGPlotPreparer:
-    def __init__(self, data_if: DataInterface, fwg_if: FWGDataInterface):
+    def __init__(self, data_if: DataInterface, fwg_subgraph: nx.DiGraph):
         self.station_coordinates = data_if.station_coordinates
-        self.fwg = fwg_if.flood_wave_graph
+        self.fwg_subgraph = fwg_subgraph
 
         self.node_positions = {}
         self.dates_dict = {}
@@ -14,7 +15,7 @@ class WNGSinkFWGPlotPreparer:
         self.create_positions_and_get_dates_dict()
 
     def create_positions_and_get_dates_dict(self):
-        for node in self.fwg.nodes:
+        for node in self.fwg_subgraph.nodes:
             base_x = self.station_coordinates[node[0]]['EOVy']
             base_y = self.station_coordinates[node[0]]['EOVx']
             self.node_positions[node] = (base_x, base_y)
